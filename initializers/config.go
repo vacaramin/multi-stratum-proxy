8e127fe02a65332aa4d97d24574181c640662801
Waqar Amin <vacaramin86@gmail.com>
2024-02-09 15:39:24 +0500

    Config JSON print test added to initializers->config.go

--- initializers/config.go
+++ initializers/config.go
@@ -50,3 +50,26 @@ func ImportConfig(filename string) (*Config, error) {
 	}
 	return &config, nil
 }
+
+// PrintConfig : Function to test the unmarshalled Config struct
+func PrintConfig(config Config) {
+	fmt.Println("Host:", config.Host)
+	fmt.Println("Event Host:", config.Event.Host)
+	fmt.Println("Event Port:", config.Event.Port)
+
+	fmt.Println("Protocols:")
+	for protocolName, protocolMap := range config.Protocols {
+		fmt.Println("\tProtocol:", protocolName)
+		for poolID, protocol := range protocolMap {
+			fmt.Println("\t\tPool ID:", poolID)
+			fmt.Println("\t\tPools:", protocol.Pools)
+			fmt.Println("\t\tFee:")
+			fmt.Println("\t\t\tWindow:", protocol.Fee.Window)
+			fmt.Println("\t\t\tPercent:", protocol.Fee.Percent)
+			fmt.Println("\t\t\tExtranonce_subscribe:", protocol.Fee.Extranonce_subscribe)
+			fmt.Println("\t\t\tPool:", protocol.Fee.Pool)
+			fmt.Println("\t\t\tWorker:", protocol.Fee.Worker)
+			fmt.Println("\t\t\tPass:", protocol.Fee.Pass)
+		}
+	}
+}

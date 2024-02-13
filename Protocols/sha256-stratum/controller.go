5690e2f3109eb58c5f6c36f91e1231123d6dcf0f
Waqar Amin <vacaramin86@gmail.com>
2024-02-13 18:53:49 +0500

    Create Stratum Proxy function values initialized

--- Protocols/sha256-stratum/controller.go
+++ Protocols/sha256-stratum/controller.go
@@ -1,5 +1,11 @@
 package sha256stratum
 
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
 // const net = require('net')
 // const Miner = require('./miner')
 // const logger = require('../../lib/logger')
@@ -31,8 +37,77 @@ type Controller struct {
 	Params      map[string]interface{}
 	LogPrefix   string
 	MinersCount int
-	Ports       map[string]string
+	Ports       map[string]interface{}
 	Miners      map[string]map[int]*Miner
+}
+
+func NewController(coin string, host string, params map[string]interface{}) Controller {
+	return Controller{
+		Coin:        coin,
+		Host:        host,
+		Params:      params,
+		LogPrefix:   strings.ToUpper(coin) + " >",
+		MinersCount: 0,
+		Ports:       make(map[string]interface{}),
+		Miners:      make(map[string]map[int]*Miner),
+	}
+}
+func (c *Controller) Init() {
+	for port, pool := range c.Params["pools"].(map[string]interface{}) {
+		c.CreateStratumProxy(port, pool)
+	}
+}
+
+func (c *Controller) CreateStratumProxy(port string, pool interface{}) {
+	// Create a TCP server
+	server, err := net.Listen("tcp", fmt.Sprintf("%s:%s", c.Host, port))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Create a new map entry for the port
+	c.Ports[port] = struct {
+		Pool   interface{}
+		Miners map[int]*Miner
+		Server net.Listener
+	}{
+		Pool:   pool,
+		Miners: make(map[int]*Miner),
+		Server: server,
+	}
+
+	// Start accepting connections
+	// go func() {
+	// 	for {
+	// 		// Accept a new connection
+	// 		conn, err := server.Accept()
+	// 		if err != nil {
+	// 			// Handle error
+	// 			continue
+	// 		}
+
+	// 		// Handle the connection in a new goroutine
+	// 		go func(conn net.Conn) {
+	// 			// Create a new Miner instance
+	// 			miner := NewMiner(c, conn, port, pool)
+
+	// 			// Connect to the pool
+	// 			miner.ConnectToPool()
+
+	// 			// Set fee (assuming fee is part of Controller's fields)
+	// 			feeWindow := c.Params["fee"].(map[string]interface{})["window"].(int)
+	// 			feePercent := c.Params["fee"].(map[string]interface{})["percent"].(float64)
+	// 			miner.SetFee(feeWindow, feePercent)
+
+	// 			// Add the miner to the map of miners for this port
+	// 			c.Ports[port].Miners[miner.ID] = miner
+	// 		}(conn)
+	// 	}
+	// }()
+
+	// // Log that the Stratum proxy has started
+	// fmt.Printf("Stratum proxy started (port: %s, pool: %v)\n", port, pool)
 }
 
 //     init() {
